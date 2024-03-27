284375bdc17902ff8dec29937707a48bdb86e073
ShineByPupil <522705046@qq.com>
2024-03-27 16:24:14 +0800

    report增删改查

--- routes/report.js
+++ routes/report.js
@@ -1,63 +1,91 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require("mysql2");
+const connection = require("../db");
+const table_name = "personal_info";
 
-router.post("/add", (req, res) => {
-  setTimeout(() => {
-    // res.send('Hello, World!');
-    res.json({ message: "(add)Hello, World!" });
-  }, 2000);
-});
+// 插入输入
+router.post("/user/add", (req, res) => {
+  const userData = req.body;
+  const query = `INSERT INTO ${table_name} SET ?`;
 
-router.post("/edit", (req, res) => {
-  // res.send('Hello, World!');
-  res.json({ message: "(edit)Hello, World!" });
-});
-
-router.post("/query", (req, res) => {
-  // 创建数据库连接
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "mysql123",
-    database: "mysql",
-  });
-
-  // 连接数据库
-  connection.connect((err) => {
-    if (err) {
-      console.error("Error connecting to database", err);
-      return;
+  connection.query(query, userData, (error, results, fields) => {
+    if (error) {
+      res.json({ status: 500, message: "操作失败", error });
+    } else {
+      res.json({ message: "新增成功", id: results.insertId });
     }
-    console.log("Connected to database");
+  });
+});
 
-    // 执行查询
-    connection.query(
-      "SELECT * FROM personal_info",
-      (error, results, fields) => {
-        if (error) {
-          console.error("Error executing query", error);
-          return;
-        }
+router.post("/user/delete", (req, res) => {
+  const id_list = req.body.id_list;
+  const query = `DELETE FROM ${table_name} WHERE id in (?)`;
 
-        res.json({
-          list: results,
-          total: results.length,
-          status: 200,
-          info: "success",
-        });
-        console.log("Query results:", results);
+  if (id_list.length === 0) {
+    res.json({ status: 500, message: "删除失败，请至少选择一项" });
+  } else {
+    connection.query(query, [id_list], (error, results, fields) => {
+      if (error) {
+        res.json({ status: 500, message: "操作失败", error });
+      } else {
+        res.json({ message: "删除成功" });
       }
-    );
-
-    // 关闭数据库连接
-    connection.end((err) => {
-      if (err) {
-        console.error("Error closing database connection", err);
-        return;
-      }
-      console.log("Closed database connection");
     });
+  }
+});
+
+// 修改数据
+router.post("/user/edit", (req, res) => {
+  const newValues = req.body;
+  const id = newValues.id;
+  delete newValues.id;
+  const query = `UPDATE ${table_name} SET ? WHERE id = ?`;
+
+  connection.query(query, [newValues, id], (error, results, fields) => {
+    if (error) {
+      res.json({ status: 500, message: "修改失败", error });
+    } else {
+      res.json({ message: "修改成功", id });
+    }
+  });
+});
+
+// 列表查询
+router.post("/user/query", (req, res) => {
+  const { current, pageSize } = req.body;
+  const offset = (current - 1) * pageSize;
+  const query = `SELECT * FROM ${table_name} LIMIT ${pageSize} OFFSET ${offset}`;
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      res.json({ status: 500, message: "操作失败", error });
+    } else {
+      connection.query(
+        `SELECT COUNT(*) AS total FROM ${table_name}`,
+        (error, countResults, fields) => {
+          if (error) {
+            res.json({ status: 500, message: "操作失败", error });
+          } else {
+            const total = countResults[0].total;
+
+            res.json({ list: results, total: total });
+          }
+        }
+      );
+    }
+  });
+});
+
+// 明细查询
+router.post("/user/detail", (req, res) => {
+  const id = req.body.id;
+  const query = `SELECT * FROM ${table_name}  WHERE id = ?`;
+
+  connection.query(query, id, (error, results, fields) => {
+    if (error) {
+      res.json({ status: 500, message: "查询失败", error });
+    } else {
+      res.json(results.length ? results[0] : {});
+    }
   });
 });
 
